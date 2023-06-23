ba9dac178fc5a78fa1c38f5a4a2be09eca235d40
lufke <nosmofui@gmail.com>
2023-06-23 12:03:23 -0400

    correccion y mejoras

--- app/screens/ProductoScreen.jsx
+++ app/screens/ProductoScreen.jsx
@@ -1,37 +1,59 @@
 import React, { useState } from 'react'
 import { Tab, TabView, Text, Icon, ListItem, FAB } from '@rneui/themed'
 import { icons } from '../components/icons'
-import { FlatList,  } from 'react-native'
+import { FlatList, View, Dimensions } from 'react-native'
 import RealmContext from '../models'
-import {categorias} from '../utils/constants'
+import { categorias } from '../utils/categories'
 
-const { useQuery } = RealmContext
-export const ProductoScreen = ({navigation}) => {
 
+const { useQuery, useRealm } = RealmContext
+export const ProductoScreen = ({ navigation }) => {
+
+    const realm = useRealm()
+    const { width, height , } = Dimensions.get('window')
     const [index, setIndex] = useState(0)
     const productos = useQuery('Producto')
-    // console.log(productos)
-
     const categoriasNombre = [...new Set(categorias.map(item => item.nombre))]
-    // console.log(categorias)
-    // console.log(categoriasNombre)
     const listasPorCategoria = categoriasNombre.map(categoria => productos.filter(item => item.categoria === categoria))
-    console.log(JSON.stringify(listasPorCategoria))
-    // console.log(listasPorCategoria)
+    
     const renderItem = ({ item }) => {
-
         return (
             <ListItem
-                key={item}
-                onPress={() => console.log(item)}
+                style={{ flex: 1, width }}
+                key={`${item._id}`}
+                onPress={() => navigation.navigate('NuevoProducto', { idProducto: item._id.toString() })}
+                onLongPress={() => deleteItem(item)}
             >
-                <Icon name={item} color="gray" />
                 <ListItem.Content>
-                    <ListItem.Title>{item.nombre}</ListItem.Title>
-                    <ListItem.Subtitle>{item.precio}</ListItem.Subtitle>
+                    <ListItem.Title><Text>{item.nombre}</Text></ListItem.Title>
+                    <ListItem.Subtitle><Text>{item.precio}</Text></ListItem.Subtitle>
                 </ListItem.Content>
-            </ListItem>)
+                <ListItem.Chevron />
+            </ListItem>
+        )
     }
+
+    const deleteItem = (item) => {
+        try {
+            realm.write(() => {
+                realm.delete(item)
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const renderItem2 = ({ item, index }) => {
+        return (
+            <View style={{ flex: 1, padding: 10, backgroundColor: index % 2 == 1 ? 'red' : 'green', width }}>
+                <Text h3>{item.nombre}</Text>
+                <Text h4>$ {item.precio}</Text>
+                <Text h4>{(index % 2).toString()}</Text>
+
+            </View>
+        )
+    }
+
     return (
         <>
             <Tab
@@ -43,7 +65,7 @@ export const ProductoScreen = ({navigation}) => {
                     return (
                         <Tab.Item
                             key={iii}
-                            title={iii.toString()}
+                            // title={item.nombre}
                             icon={{ name: item.icono, color: 'white' }}
                         />
                     )
@@ -57,25 +79,28 @@ export const ProductoScreen = ({navigation}) => {
                 {listasPorCategoria.map((lista, idx) => (
                     <TabView.Item
                         key={idx.toString()}
+                        style={{ flex: 1 }}
                     >
-                        <>
-                            <Text>id: {idx.toString()}</Text>
-                            <FlatList
-                                data={lista}
-                                keyExtractor={(_, idx) => idx.toString()}
-                                renderItem={({ item }) => <Text>{idx.toString()} - {item.nombre}</Text>}
-                            />
-                        </>
-
+                        {/* <View
+                            style={{ flex: 1 }}
+                        > */}
+                        {/* <Text>id: {idx.toString()}</Text> */}
+                        <FlatList
+                            data={lista}
+                            keyExtractor={(_, idx) => idx.toString()}
+                            renderItem={renderItem}
+                            ListEmptyComponent={<View style={{ justifyContent: 'center', alignContent: 'center' }}><Text h4>Categoría "{categoriasNombre[idx]}" sin productos</Text></View>}
+                        />
+                        {/* </View> */}
                     </TabView.Item>
                 ))}
             </TabView>
-            <FAB 
-            // onPress={()=>console.log('click')}
-            onPress={()=>navigation.navigate('NuevoProducto')}
-            icon={{name:'add', color:'green'}}
-            color='red'
-            placement='right'
+            <FAB
+                // onPress={()=>console.log('click')}
+                onPress={() => navigation.navigate('NuevoProducto', { idProducto: null, idCategoria: index })}
+                icon={{ name: 'add', color: 'green' }}
+                color='red'
+                placement='right'
             />
         </>
     )
